c956d2a5cc340032249ebffc0ae120665f98832d
martier <martier@oocl.com>
2021-08-09 16:11:07 +0800

    [WordFrequencyGame][Eric] refactor: extracted retrieveEachWordInfo method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -10,12 +10,7 @@ public class WordFrequencyGame {
         if (message.split(BLANK_SPACE).length == 1) return message + " 1";
 
         try {
-            List<WordInfo> wordInfos = Arrays
-                    .stream(message.split(BLANK_SPACE))
-                    .map(word -> new WordInfo(word, 1))
-                    .collect(Collectors.toList());
-
-            wordInfos = getEachWordInfo(wordInfos);
+            List<WordInfo> wordInfos = retrieveEachWordInfo(message);
 
             wordInfos.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
@@ -24,6 +19,14 @@ public class WordFrequencyGame {
 
             return "Calculate Error";
         }
+    }
+
+    private List<WordInfo> retrieveEachWordInfo(String message) {
+        List<WordInfo> wordInfos = Arrays
+                .stream(message.split(BLANK_SPACE))
+                .map(word -> new WordInfo(word, 1))
+                .collect(Collectors.toList());
+        return getEachWordInfo(wordInfos);
     }
 
     private String generateOutput(List<WordInfo> wordInfos) {
